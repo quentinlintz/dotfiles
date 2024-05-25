d841dfa7254d023b05a3aea550290d8fdfd8ca89
Quentin Lintz <quentinlintz@outlook.com>
2024-05-24 23:26:11 -0400

    Add completions, formatting, and gitsigns plugins

---
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,19 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				go = { "gofumpt", "goimports", "golines" },
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+	end,
+}
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -5,10 +5,10 @@ return {
 
 		null_ls.setup({
 			sources = {
+				null_ls.builtins.formatting.gofumpt,
+				null_ls.builtins.formatting.goimports_reviser,
+				null_ls.builtins.formatting.golines,
 				null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.gofumpt,
-        null_ls.builtins.formatting.goimports_reviser,
-        null_ls.builtins.formatting.golines,
 			},
 		})
 
