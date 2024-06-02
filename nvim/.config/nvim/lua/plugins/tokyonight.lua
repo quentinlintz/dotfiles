99ecd6992e79aef1ed1ab0cc679b172963269b40
Quentin Lintz <quentinlintz@outlook.com>
2024-06-02 14:14:35 -0400

    Add bufferline, background image, and transparency

--- nvim/.config/nvim/lua/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -1,10 +1,19 @@
-return  {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    vim.cmd.colorscheme "tokyonight"
-  end
-}
+return {
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("tokyonight").setup({
+			transparent = true,
+			style = "night",
+			styles = {
+				comments = { italic = true },
+				keywords = { italic = true },
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		})
 
+		vim.cmd([[colorscheme tokyonight]])
+	end,
+}
