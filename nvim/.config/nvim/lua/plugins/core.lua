d478834449047322a7613db8f1ac4efd3fc90be5
Quentin Lintz <quentinlintz@outlook.com>
2024-11-01 21:19:15 -0400

    Add session management, assembly, and SystemVerilog LSP/formatters to neovim

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -36,12 +36,22 @@ return {
           cmd = { "bundle", "exec", "rubocop", "--lsp" }, -- Project's rubocop
           root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
         },
+        verible = {
+          cmd = { "verible-verilog-ls", "--rules_config_search" },
+          filetypes = { "verilog", "systemverilog", "verilog_systemverilog" },
+        },
+        asm_lsp = {
+          filestypes = { "asm", "s", "S" },
+        },
       },
     },
   },
   {
     "stevearc/conform.nvim",
     opts = {
+      formatters_by_ft = {
+        asm = { "asmfmt" },
+      },
       formatters = {
         rubocop = {
           command = "bundle",
@@ -66,5 +76,17 @@ return {
     opts = function(_, opts)
       opts.endwise = { enable = true }
     end,
+  },
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+    keys = {
+      { "<leader>wr", "<cmd>SessionSearch<CR>", desc = "Session search" },
+      { "<leader>ws", "<cmd>SessionSave<CR>", desc = "Save session" },
+      { "<leader>wa", "<cmd>SessionToggleAutoSave<CR>", desc = "Toggle autosave" },
+    },
+    opts = {
+      allowed_dirs = { os.getenv("HOME") .. "/.dotfiles", os.getenv("HOME") .. "/dev" },
+    },
   },
 }
