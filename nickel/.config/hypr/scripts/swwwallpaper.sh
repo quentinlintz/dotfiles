cc05cc2652db8c6624523003e0b7f8881ddf4ab3
Quentin Lintz <quentinlintz@outlook.com>
2025-07-20 23:11:48 -0400

    Add all LazyExtras, set swww in script, show hidden files in Neovim

---
+++ nickel/.config/hypr/scripts/swwwallpaper.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+swww img "$HOME"/Pictures/wallpaper.png
---
+++ nickel/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    -- I want to always see hidden annd ignored files
+    picker = {
+      hidden = true,
+      ignored = true,
+    },
+  },
+}
